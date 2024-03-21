998c4dcbb88fd4536b88959bba06751e806aac44
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2024-03-21 19:28:20 +0900

    feat: GameInfo type 정의
    `

--- src/atom/gamelist.ts
+++ src/atom/gamelist.ts
@@ -1,12 +1,53 @@
 import { atom, selector } from "recoil";
 import { getGameList } from "../api/GameListApi";
 
+export type GameInfo = {
+    id: number;
+    name: string;
+    slug: string;
+    released: string;
+    background_image: string;
+    rating: number;
+    rating_top: number;
+    metacritic: number;
+    playtime: number;
+    esrb_rating?: { id: number; name: string; slug: string };
+    updated: string;
+    genres: {
+        id: number;
+        name: string;
+        slug: string;
+        games_count: number;
+        image_background: string;
+    }[];
+    stores: {
+        id: number;
+        store: {
+            id: number;
+            name: string;
+            slug: string;
+            domain: string;
+            games_count: number;
+            image_background: string;
+        };
+    }[];
+    tags: {
+        id: number;
+        name: string;
+        slug: string;
+        language: string;
+        games_count: number;
+        image_background: string;
+    }[];
+    short_screenshots: { id: number; image: string }[];
+};
+
 export const gameListState = atom({
     key: "gameListState",
     default: [],
 });
 
-export const fetchGameList = selector({
+export const fetchGameList = selector<GameInfo[]>({
     key: "fetchGameList",
     get: async () => {
         try {
