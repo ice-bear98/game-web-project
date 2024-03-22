cbf514e465b29e9c7b7c96eea5e0ece7a82d8264
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2024-03-22 14:05:51 +0900

    feat: 페이지네이션 기능 추가
    -홈 페이지 아이템 리스트에 페이지 네이션 기능추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,22 @@ import { useRecoilValue } from "recoil";
 import { fetchGameList } from "../atom/GameList";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Pagination from "react-js-pagination";
+import "../index.css";
 
 export default function Home() {
     const gameList = useRecoilValue(fetchGameList);
     const [selectedGenre, setSelectedGenre] = useState("All");
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalGamesCount = gameList.length;
     const navigate = useNavigate();
 
-    const genres = [
-        "All",
-        "Action",
-        "RPG",
-        "Shooter",
-        "Puzzle",
-        "Indie",
-        "Platformer",
-        "Adventure",
-    ];
+    console.log(gameList);
+    const handlePageChange = (pageNumber: number) => {
+        console.log(`active page is ${pageNumber}`);
+        setCurrentPage(pageNumber);
+        // 페이지 변경 시 필요한 로직 추가 (예: 새로운 데이터 로딩)
+    };
 
     const filteredGames =
         selectedGenre === "All"
@@ -25,6 +25,25 @@ export default function Home() {
             : gameList.filter((game: any) =>
                   game.genres.some((genre: any) => genre.name === selectedGenre)
               );
+
+    // 한 페이지에 표시할 아이템의 최대 개수
+    const itemCount = 15;
+
+    // 현재 페이지에서 표시할 게임 목록의 시작 인덱스
+    const indexOfLastItem = currentPage * itemCount;
+    const indexOfFirstItem = indexOfLastItem - itemCount;
+
+    // 현재 페이지에서 표시할 게임 목록
+    const currentGames = filteredGames.slice(indexOfFirstItem, indexOfLastItem);
+
+    const genres = [
+        "All",
+        ...new Set(
+            gameList
+                .map((game) => game.genres.map((genre) => genre.name))
+                .flat()
+        ),
+    ];
 
     const handleGameListDetail = (id: number) => {
         navigate(`/GameDetail/${id}`);
@@ -51,8 +70,8 @@ export default function Home() {
                 </div>
 
                 <div className="grid grid-cols-3 gap-3 ">
-                    {filteredGames.length > 0 ? (
-                        filteredGames.map((game: any) => (
+                    {currentGames.length > 0 ? (
+                        currentGames.map((game: any) => (
                             <div
                                 key={game.id}
                                 onClick={() => handleGameListDetail(game.id)}
@@ -70,6 +89,19 @@ export default function Home() {
                     ) : (
                         <p>게임 목록을 불러오는 중...</p>
                     )}
+                </div>
+                <div className="pagination-container">
+                    <Pagination
+                        activePage={currentPage}
+                        itemsCountPerPage={15}
+                        totalItemsCount={totalGamesCount}
+                        pageRangeDisplayed={5}
+                        prevPageText={"‹"}
+                        nextPageText={"›"}
+                        onChange={handlePageChange}
+                        itemClass="page-item" // 페이지 아이템에 적용될 클래스
+                        linkClass="page-link" // 페이지 링크에 적용될 클래스
+                    />
                 </div>
             </div>
         </div>
