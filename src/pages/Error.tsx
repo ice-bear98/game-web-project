efea66b772755e6e6e1c10578ecccb91c4ed80dc
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2024-03-28 13:13:43 +0900

    Style: Error 페이지 스타일 추가

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Error() {
-    return <div>Error</div>;
+    const navigate = useNavigate();
+
+    return (
+        <div className="flex flex-col justify-center items-center max-w-5xl mx-auto my-0 space-y-5">
+            <div className="text-red-700 text-8xl bold">404 Error</div>
+            <p className="text-3xl">
+                죄송합니다 현재 페이지는 찾을 수 없는 페이지입니다.
+            </p>
+            <button
+                onClick={() => {
+                    navigate("/");
+                }}
+                className=" rounded-lg bg-blue-500 text-white px-4 py-2  hover:bg-blue-700 transition duration-300"
+            >
+                Home으로 이동하기
+            </button>
+        </div>
+    );
 }
