c4f4ab279177474932d1bf66b88232463eb0f223
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2024-03-28 13:17:58 +0900

    Feat: Router Suspense 기능 추가
    -Home , GameDetail 페이지 Suspense 기능 추가
    -NewsDetail 삭제

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,11 +1,9 @@
 import DefaultLayout from "../layout/DefaultLayout";
-
 import LoginLayout from "../layout/LoginLayout";
 import Community from "../pages/Community";
 import CommunityDetail from "../pages/CommunityDetail";
 import CommunityWrite from "../pages/CommunityWrite";
 import GameDetail from "../pages/GameDetail";
-import GameNewsDetail from "../pages/GameNewsDetail";
 import GameNews from "../pages/GameNews";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -14,23 +12,46 @@ import Error from "../pages/Error";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mypage from "../pages/Mypage";
+import { Suspense } from "react";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<DefaultLayout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/GameDetail/:id" element={<GameDetail />} />
-                    <Route path="/GameNews" element={<GameNews />} />
                     <Route
-                        path="/GameNewsDetail/:id"
-                        element={<GameNewsDetail />}
+                        path="/"
+                        element={
+                            <Suspense
+                                fallback={
+                                    <div className="text-center text-5xl">
+                                        loading...
+                                    </div>
+                                }
+                            >
+                                <Home />
+                            </Suspense>
+                        }
+                    />
+                    <Route path="/GameDetail/:id" element={<GameDetail />} />
+                    <Route
+                        path="/GameNews"
+                        element={
+                            <Suspense
+                                fallback={
+                                    <div className="text-center text-5xl">
+                                        loading...
+                                    </div>
+                                }
+                            >
+                                <GameNews />
+                            </Suspense>
+                        }
                     />
                     <Route path="/mypage" element={<Mypage />} />
                     <Route path="/Community" element={<Community />} />
                     <Route
-                        path="/CommunityDetail"
+                        path="/community/:id"
                         element={<CommunityDetail />}
                     />
                     <Route
